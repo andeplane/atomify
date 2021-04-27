fe4ad65d1510e74939172f38c4c393edd4c9f255
Anders Hafreager <andershaf@gmail.com>
2021-04-27 22:24:17 +0200

    Updates

---
+++ precache-manifest.58ce187c99c53eab029b4afc6726601e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "327d560ba2aa67786f6953774a02905c",
+    "url": "/atomify/index.html"
+  },
+  {
+    "revision": "dc97a34da9ea9ce904b253c9efeba164",
+    "url": "/atomify/lammps.dc97a34d.wasm"
+  },
+  {
+    "revision": "ab04f3d0a354b296fd84",
+    "url": "/atomify/static/css/2.b1ebb4fa.chunk.css"
+  },
+  {
+    "revision": "75f46dedeef8101e4216",
+    "url": "/atomify/static/css/main.f471d78e.chunk.css"
+  },
+  {
+    "revision": "ab04f3d0a354b296fd84",
+    "url": "/atomify/static/js/2.41176535.chunk.js"
+  },
+  {
+    "revision": "f169d1684cab00c0d7c9ef79a8b8f436",
+    "url": "/atomify/static/js/2.41176535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75f46dedeef8101e4216",
+    "url": "/atomify/static/js/main.d3ce9809.chunk.js"
+  },
+  {
+    "revision": "06846e33f55a70725b1f",
+    "url": "/atomify/static/js/runtime-main.87523317.js"
+  },
+  {
+    "revision": "b13daaad214ef227a36fefd95d924380",
+    "url": "/atomify/static/media/codicon.b13daaad.ttf"
+  }
+]);
