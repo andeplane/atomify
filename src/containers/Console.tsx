f5022e6d823abf4450ab86a0666cfc2bb885f0ed
Anders Hafreager <andershaf@gmail.com>
2022-10-15 23:38:46 +0200

    Scrolling to bottom in console

--- src/containers/Console.tsx
+++ src/containers/Console.tsx
@@ -1,13 +1,21 @@
 import MonacoEditor from 'react-monaco-editor'
-
+import {useEffect, useRef} from 'react'
 import { useStoreState } from '../hooks';
 
 const Console = () => {
   const lammpsOutput = useStoreState(state => state.simulation.lammpsOutput)
+  const editorRef = useRef<any>(null)
   const options = {
     selectOnLineNumbers: true,
     readOnly: true
   };
+  useEffect(() => {
+    const editor = editorRef.current?.editor
+    if (editor) {
+      editor.revealLine(editor.getModel().getLineCount())
+    }
+  }, [lammpsOutput])
+
   return (
     <MonacoEditor
       height="100vh"
@@ -15,6 +23,7 @@ const Console = () => {
       theme="vs-dark"
       value={lammpsOutput.join("\n")}
       options={options}
+      ref={editorRef}
     />
   )
 }
