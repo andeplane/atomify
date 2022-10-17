954a0caf226c89c96892c598cf8679990387bfac
Anders Hafreager <andershaf@gmail.com>
2022-10-17 19:55:03 +0200

    Added new preview page of analyze

--- src/containers/Analyze.tsx
+++ src/containers/Analyze.tsx
@@ -1,5 +1,14 @@
+import {Layout} from 'antd'
+const {Header} = Layout
 
 const Analyze = () => {
-  return (<>Analyze</>)
+  
+  return (
+    <>
+    <Header className="site-layout-background" style={{ fontSize: 25 }}>
+      Examples 
+    </Header>
+    <p>Analyze of computes, variables and fixes is coming soon.</p>
+    </>)
 }
 export default Analyze
