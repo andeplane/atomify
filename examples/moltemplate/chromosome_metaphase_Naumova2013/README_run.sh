91c8a962821d7c5b12e627a912ee4db71bd4a5f8
andeplane <andeplane@users.noreply.github.com>
2022-10-17 17:15:27 +0000

    deploy: 45a185b39efe419a2ac5916ff2da0732b299d642

--- examples/moltemplate/chromosome_metaphase_Naumova2013/README_run.sh
+++
@@ -1,7 +0,0 @@
-# Run lammps using the following 3 commands:
-# (assuming "lmp_mpi" is the name of your LAMMPS binary)
-
-mpirun -n 4 lmp_mpi -i run.in.min
-mpirun -n 4 lmp_mpi -i run.in.stage1
-mpirun -n 4 lmp_mpi -i run.in.stage2
-
