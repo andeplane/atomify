265340a06f6ee4cf7864d40e9299782a22098389
Anders Hafreager <andershaf@gmail.com>
2022-10-18 19:48:07 +0200

    Fixed linting

--- src/containers/Analyze.tsx
+++ src/containers/Analyze.tsx
@@ -1,21 +1,18 @@
-import {Layout} from 'antd'
 import Iframe from 'react-iframe'
-
-const {Header} = Layout
 
 const Analyze = () => {
   return (
     <>
-    <div style={{height: '100vh', width: '100%'}}>
-    <Iframe url="/atomify/jupyter/index.html"
-        width="100%"
-        height="100%"
-        id=""
-        className=""
-        display="block"
-        position="relative"/>
-    </div>
+      <div style={{height: '100vh', width: '100%'}}>
+      <Iframe url="/atomify/jupyter/index.html"
+          width="100%"
+          height="100%"
+          id=""
+          className=""
+          display="block"
+          position="relative"/>
+      </div>
     </>
-    )
+  )
 }
 export default Analyze
