608d4a76218cfbde0ae204f9cf7078d7acee6dc8
Anders Hafreager <andershaf@gmail.com>
2022-10-29 18:31:04 +0200

    Fixed some state issues

--- src/modifiers/ColorModifierSettings.tsx
+++ src/modifiers/ColorModifierSettings.tsx
@@ -1,5 +1,5 @@
 import {Modal, Select} from 'antd'
-import { useStoreState } from '../hooks';
+import { useStoreState, useStoreActions } from '../hooks';
 import {useCallback} from 'react'
 import ColorModifier from './colormodifier';
 const {Option, OptGroup} = Select
@@ -8,21 +8,22 @@ const ColorModifierSettings = ({onClose}:{onClose: () => void}) => {
   const computes = useStoreState(state => state.simulationStatus.computes)
   // const fixes = useStoreState(state => state.simulationStatus.fixes)
   const postTimestepModifiers = useStoreState(state => state.processing.postTimestepModifiers)
+  const setParticleStylesUpdated = useStoreActions(actions => actions.render.setParticleStylesUpdated)
   const colorModifier = postTimestepModifiers.filter(modifier => modifier.name==="Colors")[0] as ColorModifier
   const perAtomComputes = computes.filter(compute => compute.isPerAtom)
-  console.log("These are the computes: ", perAtomComputes)
   
   const onChange = useCallback((value: string) => {
     if (value !== "type") {
       colorModifier.computeName = value
     } else {
       colorModifier.computeName = undefined
+      setParticleStylesUpdated(true)
     }
-  }, [colorModifier])
-  
+  }, [colorModifier, setParticleStylesUpdated])
+  const defaultValue = colorModifier.computeName ? colorModifier.computeName : 'type'
   return (
     <Modal title="Particle color settings" open footer={null} onCancel={onClose}>
-      <Select defaultValue="type" style={{ width: 200 }} onChange={onChange}>
+      <Select defaultValue={defaultValue} style={{ width: 200 }} onChange={onChange}>
         <Option value="type">Particle type</Option>
         <OptGroup label="Computes">
           {perAtomComputes.map(compute => (
