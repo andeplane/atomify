5aa0f378b8e21e3f96225c5445da75bd3bfd8219
Anders Hafreager <anders.hafreager@cognite.com>
2025-08-22 09:16:29 +0200

    feat: add missing Examples.css import
    - Add Examples.css file that was created but never committed
    - Import Examples.css in Examples.tsx to enable modern styling
    - This completes the modern card design for the examples page

--- src/containers/Examples.tsx
+++ src/containers/Examples.tsx
@@ -11,6 +11,7 @@ import ReactMarkdown from "react-markdown";
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 import "katex/dist/katex.min.css";
+import "./Examples.css";
 
 const { Option } = Select;
 
